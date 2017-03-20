417dd9a60df08a305446c52f23aae79450ba43ab
Shilp_m <shilp_m@macmini.local>
2017-03-20 12:57:05 +0530

    Initial Commit

---
+++ Validex/Validex.h
@@ -0,0 +1,19 @@
+//
+//  Validex.h
+//  Validex
+//
+//  Created by Shilp_m on 3/20/17.
+//  Copyright © 2017 DryRun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Validex.
+FOUNDATION_EXPORT double ValidexVersionNumber;
+
+//! Project version string for Validex.
+FOUNDATION_EXPORT const unsigned char ValidexVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Validex/PublicHeader.h>
+
+
